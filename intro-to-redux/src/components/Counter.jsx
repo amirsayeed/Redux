cc64a93c32ee1c77c6b5e5d1ca334915018c516a
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-09-01 15:05:11 +0600

    used redux state and actions in counter component

--- intro-to-redux/src/components/Counter.jsx
+++ intro-to-redux/src/components/Counter.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementByAmount } from '../redux/features/counter/counterSlice';
 
 const Counter = () => {
+    const {count} = useSelector((state) => state.counter)
+    const dispatch = useDispatch()
+
     return (
         <div>
-            <h1>Counter</h1>
+            <div className='flex gap-5'>
+                <button className='p-4 bg-green-400 text-white rounded-md' onClick={()=>dispatch(increment())}>Increment</button>
+                <button className='p-4 bg-red-400 text-white rounded-md' onClick={()=>dispatch(decrement())}>Decrement</button>
+                <button className='p-4 bg-blue-400 text-white rounded-md' onClick={()=>dispatch(incrementByAmount(5))}>Increment By Value</button>
+            </div>
+            <span className='text-xl'>{count}</span>
         </div>
     );
 };
