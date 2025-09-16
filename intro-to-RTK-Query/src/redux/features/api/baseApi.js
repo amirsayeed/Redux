325e5f939116b5cb2a20c1b9ddafbd2578ea4952
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-09-16 14:52:46 +0600

    connecting RTK query with store and building the first query GET

---
+++ intro-to-RTK-Query/src/redux/features/api/baseApi.js
@@ -0,0 +1,22 @@
+import {
+    fetchBaseQuery
+} from "@reduxjs/toolkit/query";
+import {
+    createApi
+} from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://jsonplaceholder.typicode.com'
+    }),
+    endpoints: (builder) => ({
+        getPosts: builder.query({
+            query: () => '/posts',
+        }),
+    }),
+});
+
+export const {
+    useGetPostsQuery
+} = baseApi;
---
+++ intro-to-RTK-Query/src/redux/store.js
@@ -0,0 +1,15 @@
+import {
+    configureStore
+} from '@reduxjs/toolkit'
+import {
+    baseApi
+} from './features/api/baseApi'
+
+export const store = configureStore({
+    reducer: {
+        [baseApi.reducerPath]: baseApi.reducer,
+    },
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(baseApi.middleware),
+})
