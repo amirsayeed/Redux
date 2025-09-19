690c55f84092c214ad8e486e122b18be629d9be1
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-09-20 01:37:38 +0600

    basic navbar added

--- Projects/shopping-cart-redux-toolkit/src/layouts/RootLayout.jsx
+++ Projects/shopping-cart-redux-toolkit/src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../components/Navbar';
 
 const RootLayout = () => {
     return (
         <div>
+            <Navbar/>
             <div className='max-w-7xl mx-auto px-2'>
                 <Outlet/>
             </div>
