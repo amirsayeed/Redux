f6b0651abf888fb59aa2241bddc999e094c7ef18
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-09-21 18:10:38 +0600

    created product routes and fixed layout strucure and navbar

--- Projects/shopping-cart-redux-toolkit/src/pages/Home/Home.jsx
+++ Projects/shopping-cart-redux-toolkit/src/pages/Home/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2 className='text-2xl'>Welcome to Shopping Cart!</h2>
+        <div className='grid lg:grid-cols-3 grid-cols-2'>
+            <div className='col-span-2'>Display Products</div>
+            <div>Add Product</div>
         </div>
     );
 };
