ca4d7edffecf6a6c86fc534b7ec3bf58c7c0c44e
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-09-21 22:12:02 +0600

    created redux store and productSlice added to the store

--- Projects/shopping-cart-redux-toolkit/src/pages/Home/Home.jsx
+++ Projects/shopping-cart-redux-toolkit/src/pages/Home/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
     return (
         <div className='grid lg:grid-cols-3 grid-cols-2'>
-            <div className='col-span-2'>Display Products</div>
+            <div className='col-span-2'>
+                Display Products
+            </div>
             <div>Add Product</div>
         </div>
     );
