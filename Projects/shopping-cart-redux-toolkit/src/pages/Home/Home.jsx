3945e801d142eb233fe2974fce02d24bafa2da9b
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-09-23 12:25:37 +0600

    add product form created using react hook form and data added action dispatched

--- Projects/shopping-cart-redux-toolkit/src/pages/Home/Home.jsx
+++ Projects/shopping-cart-redux-toolkit/src/pages/Home/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import ProductCard from '../Products/ProductCard';
+import AddProduct from '../../components/AddProduct';
 
 
 const Home = () => {
     const products = useSelector((state) => state.products);
-    console.log(products);
+    // console.log(products);
 
     return (
-        <div className='grid lg:grid-cols-3 grid-cols-2'>
+        <div className='grid lg:grid-cols-3 grid-cols-2 gap-4 my-10'>
             <div className='col-span-2'>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
                 {
                     products.length ? products.map((product,idx)=>
                     <ProductCard key={idx} product={product}/>
@@ -18,7 +19,7 @@ const Home = () => {
                 }
                 </div>
             </div>
-            <div>Add Product</div>
+            <div className='col-span-full lg:col-span-1'><AddProduct/></div>
         </div>
     );
 };
--- Projects/shopping-cart-redux-toolkit/src/pages/Products/ProductCard.jsx
+++ Projects/shopping-cart-redux-toolkit/src/pages/Products/ProductCard.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 
 const ProductCard = ({product}) => {
     const {name,image, price, category} = product;
+
+    const handleAddToCart = () =>{
+
+    }
+
     return (
-        <div className="card bg-base-100 shadow-sm">
+        <div className="card bg-base-100 shadow-md">
         <figure>
             <img
-            src={image} className='h-[200px] w-full'
+            src={image} className='h-48 w-full object-cover'
             alt="Shoes" />
         </figure>
         <div className="card-body">
             <h2 className="card-title">{name}</h2>
-            <p>Price: {price}</p>
-            <p>Category: {category}</p>
-            <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-            </div>
+            <p className='text-base font-medium'>Category: {category}</p>
+        <div className="card-actions items-center justify-end">
+            <p className="text-xl font-semibold">${price}</p>
+            <button onClick={handleAddToCart} className="btn bg-violet-500 text-white">Add to Cart
+            </button>
+        </div>
         </div>
         </div>
     );
